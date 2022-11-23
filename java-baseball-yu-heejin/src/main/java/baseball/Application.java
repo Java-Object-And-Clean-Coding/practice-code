5c3d3c4f5e86bd262e4fcced1a4538df8e1af97e
yu-heejin <jkgt0521@naver.com>
2022-11-23 21:17:09 +0900

    refactor: 코드 리팩토링 1. 메소드는 한 가지 일만 수행한다. 2. 명확한 이름을 제시한다.

--- java-baseball-yu-heejin/src/main/java/baseball/Application.java
+++ java-baseball-yu-heejin/src/main/java/baseball/Application.java
@@ -10,44 +10,56 @@ public class Application {
     private static int[] gameResultCount;
     private static final int STRIKE_INDEX = 0;
     private static final int BALL_INDEX = 1;
-    private static List<Integer> randomNumbers;
+    private static List<Integer> randomNumbers = new ArrayList<>();
     private static final char CHAR_TO_NUMBER = '0';
     private static String gameContinueAnswer = "1";
 
 
     public static void makeRandomNumber() {
-        randomNumbers = new ArrayList<>();
-
         while (randomNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!randomNumbers.contains(randomNumber)) {
-                randomNumbers.add(randomNumber);
-            }
+            insertRandomNumber(randomNumber);
+        }
+    }
+
+    public static void insertRandomNumber(int randomNumber) {
+        if (!randomNumbers.contains(randomNumber)) {
+            randomNumbers.add(randomNumber);
         }
     }
 
     public static void printGameResult() {
+        System.out.println(returnGameResult());
+    }
+
+    public static String returnGameResult() {
         if (gameResultCount[BALL_INDEX] == 0 && gameResultCount[STRIKE_INDEX] > 0) {
-            System.out.println(gameResultCount[STRIKE_INDEX] + "스트라이크");
-        } else if (gameResultCount[BALL_INDEX] > 0 && gameResultCount[STRIKE_INDEX] > 0) {
-            System.out.println(gameResultCount[BALL_INDEX] + "볼 " + gameResultCount[STRIKE_INDEX] + "스트라이크");
-        } else if (gameResultCount[STRIKE_INDEX] == 0 && gameResultCount[BALL_INDEX] > 0) {
-            System.out.println(gameResultCount[BALL_INDEX] +"볼");
-        } else {
-            System.out.println("낫싱");
+            return gameResultCount[STRIKE_INDEX] + "스트라이크";
         }
+        if (gameResultCount[BALL_INDEX] > 0 && gameResultCount[STRIKE_INDEX] > 0) {
+            return gameResultCount[BALL_INDEX] + "볼 " + gameResultCount[STRIKE_INDEX] + "스트라이크";
+        }
+        if (gameResultCount[STRIKE_INDEX] == 0 && gameResultCount[BALL_INDEX] > 0) {
+            return gameResultCount[BALL_INDEX] +"볼";
+        }
+        return "낫싱";
     }
 
     public static void checkGameResult(List<Integer> randomNumbers, String number) {
         for (int i = 0; i < NUMBER_SIZE; i++) {
             int dividedNumber = returnDividedNumber(number, i);
-            if (randomNumbers.get(i) == dividedNumber) {
-                gameResultCount[STRIKE_INDEX]++;
-                continue;
-            }
-            if (randomNumbers.contains(dividedNumber)) {
-                gameResultCount[BALL_INDEX]++;
-            }
+            countGameResult(i, dividedNumber);
+        }
+    }
+
+    public static void countGameResult(int index, int dividedNumber) {
+        if (randomNumbers.get(index) == dividedNumber) {
+            gameResultCount[STRIKE_INDEX]++;
+            return;
+        }
+
+        if (randomNumbers.contains(dividedNumber)) {
+            gameResultCount[BALL_INDEX]++;
         }
     }
 
@@ -55,25 +67,41 @@ public class Application {
         return number.charAt(index) - CHAR_TO_NUMBER;
     }
 
+    public static String inputUserNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String number = Console.readLine();
+
+        return number;
+    }
+
+    public static void inputGameContinueAnswer() {
+        if (gameResultCount[STRIKE_INDEX] == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            gameContinueAnswer = Console.readLine();
+
+            if (gameContinueAnswer.equals("1")) {
+                randomNumbers.clear();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-        makeRandomNumber();
-        System.out.println(randomNumbers);
 
         while (gameContinueAnswer.equals("1")) {
             gameResultCount = new int[]{0, 0};
-            System.out.print("숫자를 입력해주세요 : ");
-            String number = Console.readLine();
+            makeRandomNumber();
+            System.out.println(randomNumbers);
+
+            String number = inputUserNumber();
 
             checkGameResult(randomNumbers, number);
 
             printGameResult();
 
-            if (gameResultCount[STRIKE_INDEX] == 3) {
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                gameContinueAnswer = Console.readLine();
-            }
+            inputGameContinueAnswer();
         }
     }
 }
