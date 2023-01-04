16174a1c640cc73782395db794f3cb41a20ec6c2
yu-heejin <jkgt0521@naver.com>
2023-01-04 11:17:07 +0900

    feat: 랜덤 숫자 생성하기 기능 추가

--- java-baseball-yu-heejin/src/main/java/baseball/Application.java
+++ java-baseball-yu-heejin/src/main/java/baseball/Application.java
@@ -1,16 +1,20 @@
 package baseball;
 
+import baseball.controller.NumberBaseballGameController;
 import baseball.view.InputUI;
 import baseball.view.OutputView;
 
 public class Application {
     private static final OutputView OUTPUT_VIEW = new OutputView();
     private static final InputUI INPUT_UI = new InputUI();
+    private static final NumberBaseballGameController NUMBER_BASEBALL_GAME_CONTROLLER = new NumberBaseballGameController();
+
     public static void main(String[] args) {
         OUTPUT_VIEW.printGameStartMessage();
+
         OUTPUT_VIEW.printInputNumberMessage();
         String number = INPUT_UI.inputUserNumber();
 
-        System.out.println(number);
+        NUMBER_BASEBALL_GAME_CONTROLLER.startGame(number);
     }
 }
--- java-baseball-yu-heejin/src/main/java/baseball/controller/NumberBaseballGameController.java
+++ java-baseball-yu-heejin/src/main/java/baseball/controller/NumberBaseballGameController.java
@@ -1,5 +1,13 @@
 package baseball.controller;
 
-public class NumberBaseballGameController {
+import baseball.model.service.NumberBaseballGameService;
 
+import java.util.List;
+
+public class NumberBaseballGameController {
+    private final NumberBaseballGameService BASEBALL_GAME_SERVICE = new NumberBaseballGameService();
+    public void startGame(String number) {
+        List<Integer> randomNumbers = BASEBALL_GAME_SERVICE.getRandomNumbers();
+        System.out.println(randomNumbers);
+    }
 }
