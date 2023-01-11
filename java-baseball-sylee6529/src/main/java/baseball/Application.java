1683d1b8eb3a253a976f52a25877b5cb851113f9
sylee <sy>
2023-01-11 15:59:15 +0900

    feat: 사용자 입력 받은 숫자의 결과 반환

--- java-baseball-sylee6529/src/main/java/baseball/Application.java
+++ java-baseball-sylee6529/src/main/java/baseball/Application.java
@@ -8,14 +8,15 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+// TODO : Model, View, Controller로 나누어보기
 public class Application {
     private static final int NUMBER_SIZE = 3;
 
-    private static ArrayList<Integer> randomNumberList = new ArrayList<>();
-    private static String inputNumber = "";
-    private static boolean IS_GAME_RESTARTED = true;
-
     public static void main(String[] args) {
+        ArrayList<Integer> randomNumberList;
+        String inputNumber;
+        boolean IS_GAME_RESTARTED = true;
+
         while(IS_GAME_RESTARTED) {
             randomNumberList = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 9, NUMBER_SIZE));
             System.out.println("숫자 야구 게임을 시작합니다.");
@@ -24,19 +25,19 @@ public class Application {
             inputNumber = Console.readLine();
             validateInputNumber(inputNumber);
 
-            String result = getGameResult(inputNumber);
-            System.out.println(result);
+            printGameResult(randomNumberList, inputNumber);
 
             IS_GAME_RESTARTED = checkIsGameRestart(inputNumber);
         }
     }
 
+    // TODO : Input Model Class 분리 및 내부 메소드로 추가
     static void validateInputNumber(String input) {
         if(input.length() != NUMBER_SIZE) {
             throw new IllegalArgumentException("3자리 숫자가 아닙니다.");
         }
 
-        ArrayList<String> numberList = new ArrayList<>(Arrays.asList(inputNumber.split("")));
+        ArrayList<String> numberList = convertStringNumberToArrayListOfNumber(input);
         Set<String> numberSet = new HashSet<>(numberList);
 
         if(numberList.size() != numberSet.size()) {
@@ -48,12 +49,34 @@ public class Application {
         }
     }
 
-    static String getGameResult(String input) {
-        return "";
+    // TODO : Output Model Class 분리 및 멤버 변수 및 메소드로 추가
+    static void printGameResult(ArrayList<Integer> randomNumberList, String input) {
+        ArrayList<String> numberList = convertStringNumberToArrayListOfNumber(input);
+        int ballCount = getBallCount(randomNumberList, numberList);
+        int strikeCount = getStrikeCount(randomNumberList, numberList);
+
+        if(ballCount == 0 && strikeCount == 0) {
+            System.out.println("낫싱");
+        }
+        else {
+            System.out.println(ballCount + "카운트 " + strikeCount + "스트라이크");
+        }
     }
 
+    static int getBallCount(ArrayList<Integer> randomNumberList, ArrayList<String> numberList) {
+        return -1;
+    }
+
+    static int getStrikeCount(ArrayList<Integer> randomNumberList, ArrayList<String> numberList) {
+        return -1;
+    }
+
+    // TODO : 더 작은 단위로 메소드 분리
     static boolean checkIsGameRestart(String input) {
         return false;
     }
 
+    static ArrayList<String> convertStringNumberToArrayListOfNumber(String number) {
+        return new ArrayList<>(Arrays.asList(number.split("")));
+    }
 }
