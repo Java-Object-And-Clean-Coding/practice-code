8bd26c30bd3de13a4a5930b9ca093f67a561cc3a
sylee <sy>
2023-01-11 10:26:46 +0900

    feat: 컴퓨터 랜덤 숫자 생성

--- java-baseball-sylee6529/src/main/java/baseball/Application.java
+++ java-baseball-sylee6529/src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
 public class Application {
+    private static final int MAX_NUMBER_LIST_SIZE = 3;
+
+    private static ArrayList<Integer> randomNumberList = new ArrayList<>();
+    private static String inputNumber = "";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        randomNumberList = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 9, MAX_NUMBER_LIST_SIZE));
     }
+
+
+
 }
